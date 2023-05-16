1981d09d51298ecd133b9563c7b121e89259502a
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-16 19:43:24 -0400

    Add variables

---
+++ app.js
@@ -0,0 +1,15 @@
+const computerScore = document.getElementById('computer-score');
+const playerScore = documnet.getElementById('player-score');
+
+const playerRock = document.getElementById("player-rock"); 
+const playerPaper = document.getElementById("player-paper");
+const playerSciccors = document.getElementById("player-scissors");
+
+const computerRock = document.getElementById("computer-rock");
+const computerPaper = document.getElementById("computer-paper");
+const computerSciccors = document.getElementById("computer-scissors");
+
+const startGame = document.getElementById("start-game");
+const resetGame = document.getElementById("reset-game");
+const nextRound = document.getElementById("next-round");
+
